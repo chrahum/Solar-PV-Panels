650f0e9cb7da591bc39ff92c366db53f43a655b5
Migiuel De La Toba <mitoba@hotmail.com>
2016-10-24 14:41:01 -0700

    ng repeat for options

--- SolarProject/controller/myController.js
+++ SolarProject/controller/myController.js
@@ -1,8 +1,46 @@
 angular.module("myApp").controller("myController", function($scope, $http) {
 
         $http.get("http://localhost:8080/json/equipment.json")
-            .then(function(response) {
-                console.log(response.data.panels[0].brand[0])
+.then(function (response) {
+                $scope.panels = response.data.panels[0].brand; 
+                $scope.mounts = response.data.mountingBracket; 
+                $scope.inver = response.data.inverter; 
+
             }); 
 
-});
+$http.get("http://localhost:8080/json/contractors.json")
+.then(function (response) {
+                $scope.contractors = response.data.contractor; 
+            }); 
+
+
+$http.get("http://localhost:8080/json/engineer.json")
+.then(function (response) {
+                $scope.engineer = response.data; 
+                console.log($scope.engineer);
+            }); 
+
+
+
+
+
+$scope.selectedPanel = ""; 
+
+$scope.selectedPanel = function(panel) {
+    $scope.currentPanel = panel; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //the end do not delete
